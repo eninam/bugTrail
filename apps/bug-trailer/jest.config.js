12a21019456d78c028c7bfc1cf6fd111a364aabe
eninam <megantako@gmail.com>
2021-06-28 17:03:00 -0400

    initial commit

---
+++ apps/bug-trailer/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'bug-trailer',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/bug-trailer',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/bug-trailer', '<rootDir>/apps/api'],
+};
